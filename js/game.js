707f74fafaeffbbba55eeaa13a8d7043ee84a285
Alexis Seals <blarfy6216@gmail.com>
2023-03-06 14:09:56 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -67,29 +67,25 @@ function placeDie(column, die, player) {
     }
 }
 
+function checkEnd() {
+    //check if all columns are full
+    let p1Full = true;
+    let p2Full = true;
+    for(let i = 0; i < 3; i++) {
+        if (p1Dice[i].length != 3) p1Full = false;
+        if (p2Dice[i].length != 3) p2Full = false;
+    }
+    if (p1Full || p2Full) return true;
+    else return false;
+}
+
 function findWinner() {
-    let p1Score = 0;
-    let p2Score = 0;
-    p1Dice.forEach(column => {
-        for(let i = 0; i < column.length; i++) {
-            let scored = false;
-            for(let j = i + 1; j < column.length; j++) {
-                if (column[i] == column[j] && j > i) {
-                    if (i == 0 && j == 1 && column[2] == column[i]){
-                        //triple
-                        p1Score += column[i] * 9;
-                        scored = true;
-                        i = column.length;
-                    } else {
-                        //double
-                        p1Score += column[i] * 4;
-                        scored = true;
-                    }
-                }
-            }
-            if (!scored) p1Score += column[i];
-        }
-    });
+    let p1Score = score(p1Dice);
+    let p2Score = score(p2Dice);
+    if (p1Score == p2Score) return 3;
+    else if (p1Score > p2Score) return 1;
+    else if (p2Score > p1Score) return 2;
+    else return 0;
 }
 
 
@@ -99,6 +95,8 @@ function findWinner() {
 // new die is created at location
 
 // dicetraydie.addEventListener('click', function() {})
+
+
 function score(diceArray) {
     let score = 0;
     diceArray.forEach(column => {
