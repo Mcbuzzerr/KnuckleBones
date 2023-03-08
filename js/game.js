2845173834ca2b678694ec99cf2d4cf695a43731
Alex <mcbuzzer@gmail.com>
2023-03-08 13:28:02 -0700

    Scoring and placeDie Modifications

--- js/game.js
+++ js/game.js
@@ -1,13 +1,25 @@
-let p1Dice = [[],[],[]];
-let p2Dice = [[],[],[]];
+let p1Dice = [[null, null, null],[null, null, null],[null, null, null]];
+let p2Dice = [[null, null, null],[null, null, null],[null, null, null]];
 let currentRoll = 0;
+let turns = 0;
 
 
 function visRoll(){
     //roll 30 times
     //update on final roll
     let rolls = 0;
-    let trayDie = document.getElementById("trayDie");
+    let trayDie = document.createElement("div");
+    trayDie.classList.add("die");
+    trayDie.id = "trayDie";
+    for (let i = 1; i <= 6; i++) {
+        let pip = document.createElement("div");
+        pip.classList.add("pip");
+        pip.classList.add("centerInsides");
+        pip.innerHTML = i;
+        trayDie.appendChild(pip);
+    }
+
+    document.getElementById("diceTray").appendChild(trayDie);
     let endRoll = rollDie();
 
     let interval = setInterval(function(){
@@ -24,18 +36,142 @@ function visRoll(){
             trayDie.classList.add("die-" + endRoll);
         }
     }, 73);
-    document.getElementById("trayDie").addEventListener('mouseDown', (event) => { clickDragDie(event) });
+    trayDie.addEventListener('mousedown', clickDragDie);
     return endRoll;
 }
 
 const clickDragDie = (event) => {
+    console.log("clickDragDie() called");
+    if (event.target.id != "trayDie") return;
     let die = event.target;
+    let tray = document.getElementById("diceTray");
 
+    document.body.appendChild(die);
+    die.style.position = 'absolute';
+    die.style.zIndex = 1000;
+    die.style.top = event.clientY - 30 + 'px';
+    die.style.left = event.clientX - 30 + 'px';
 
+    const onMouseUp = (event) => {
+        //Place die in row
+        document.body.removeChild(die);
+        if (document.elementFromPoint(event.clientX, event.clientY).classList.contains("item")) {
+            let tile = document.elementFromPoint(event.clientX, event.clientY);
+            document.body.appendChild(die);
+            let tileID = tile.id.split("-");
+            let column = tileID[2];
+            let row = tileID[1];
+            let tileOwner = tileID[0];
+            console.log(tileOwner);
+            console.log(turns);
+            console.log(turns % 2);
+            if (turns % 2 == 0 && tileOwner == "p1") {
+                let value = die.classList[1].split("-")[1];
+                die.id = ""
+                placeDie(column, row, value, true)
+                render()
+                checkEnd()
+                alert(score(p1Dice))
+            }
+            else if (turns % 2 == 1 && tileOwner == "p2") {
+                let value = die.classList[1].split("-")[1];
+                die.id = ""
+                placeDie(column, row, value, false)
+                render()
+                checkEnd()
+                alert(score(p2Dice))
+            } else {
+                tray.appendChild(die);
+                die.style.position = 'relative';
+                die.style.zIndex = 0;
+                die.style.top = 0;
+                die.style.left = 0;
+                document.removeEventListener('mouseup', onMouseUp);
+                document.removeEventListener('mousemove', onMouseMove);
+                console.log("not your turn");
+                return;
+            }
+            die.remove();
+            // tile.appendChild(die);
+            // die.style.position = 'relative';
+            // die.style.zIndex = 0;
+            // die.style.top = 0;
+            // die.style.left = 0;
+            // console.log("column: " + column + " row: " + row);
+            
+
+            document.removeEventListener('mouseup', onMouseUp);
+            document.removeEventListener('mousemove', onMouseMove);
+            die.removeEventListener('mousedown', clickDragDie);
+            turns++;
+        } else {
+            tray.appendChild(die);
+            die.style.position = 'relative';
+            die.style.zIndex = 0;
+            die.style.top = 0;
+            die.style.left = 0;
+            document.removeEventListener('mouseup', onMouseUp);
+            document.removeEventListener('mousemove', onMouseMove);
+            console.log("not a tile");
+        }
+    }
+
+    const onMouseMove = (event) => {
+        die.style.top = (event.clientY - 30)+ 'px';
+        die.style.left = (event.clientX - 30)+ 'px';
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+}
+
+const render = () => {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let tile = document.getElementById("p1-" + i + "-" + j);
+            tile.innerHTML = "";
+            if (p1Dice[i][j] == null) {
+                continue;
+            }
+
+            let die = document.createElement("div");
+            die.classList.add("die");
+            die.classList.add("die-" + p1Dice[i][j]);
+            for (let i = 1; i <= 6; i++) {
+                let pip = document.createElement("div");
+                pip.classList.add("pip");
+                pip.classList.add("centerInsides");
+                pip.innerHTML = i;
+                die.appendChild(pip);
+            }
+
+            tile.appendChild(die);
+        }
+    }
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (p2Dice[i][j] == null) continue;
+            let tile = document.getElementById("p2-" + i + "-" + j);
+            tile.innerHTML = "";
+
+            let die = document.createElement("div");
+            die.classList.add("die");
+            die.classList.add("die-" + p2Dice[i][j]);
+            for (let i = 1; i <= 6; i++) {
+                let pip = document.createElement("div");
+                pip.classList.add("pip");
+                pip.classList.add("centerInsides");
+                pip.innerHTML = i;
+                die.appendChild(pip);
+            }
+
+            tile.appendChild(die);
+        }
+    }
 }
 
 function rollDie() {
-        return Math.floor(Math.random() * 6) + 1;
+    return Math.floor(Math.random() * 6) + 1;
 }
 
 /**
@@ -50,39 +186,39 @@ function rollDie() {
  * @returns 
  * true if successful, false if not
  */
-function placeDie(column, die, player) {
+function placeDie(column, row, die, player) {
     if (player) {
-        if (p1Dice[column].length < 3) {
-            p1Dice[column].push(die);
-            for(let i = 0; i < p2Dice[column].length; i++) {
-                if (die == p2Dice[column][i]) {
-                    p2Dice[column].splice(i,1);
-                    break;
-                }   
-            }
-            return true;
-        } else return false;
+        console.log(column)
+        p1Dice[row][column] = Number(die);
+        for(let i = 0; i < p2Dice[row].length; i++) {
+            if (die == p2Dice[row][i]) {
+                p2Dice[row][i] = null;
+                continue;
+            }   
+        }
+        return true;
     } else {
-        if (p2Dice[column].length < 3) {
-            p2Dice[column].push(die);
-            for(let i = 0; i < p1Dice[column].length; i++) {
-                if (die == p1Dice[column][i]) {
-                    p1Dice[column].splice(i,1);
-                    break;
-                }   
-            }
-            return true;
-        } else return false;
+        p2Dice[row][column] = Number(die);
+        for(let i = 0; i < p1Dice[row].length; i++) {
+            if (die == p1Dice[row][i]) {
+                p1Dice[row][i] = null;
+                continue;
+            }   
+        }
+        return true;
     }
 }
 
+// UNTESTED
 function checkEnd() {
     //check if all columns are full
     let p1Full = true;
     let p2Full = true;
     for(let i = 0; i < 3; i++) {
-        if (p1Dice[i].length != 3) p1Full = false;
-        if (p2Dice[i].length != 3) p2Full = false;
+        for (let j = 0; j < 3; j++) {
+            if (p1Dice[i][j] == null) p1Full = false;
+            if (p2Dice[i][j] == null) p2Full = false;
+        }
     }
     if (p1Full || p2Full) return true;
     else return false;
@@ -97,16 +233,32 @@ function findWinner() {
     else return 0;
 }
 
-
-// Dicetray die
-// roll button
-// click drag from dicetray die to desired location
-// new die is created at location
-
-
-
-
 function score(diceArray) {
+    let score = 0;
+    for (let i = 0; i < diceArray.length; i++) {
+        let rowScore = 0;
+        let rowStr = diceArray[i].join("");
+        let triple = false;
+        let double = false;
+        for (let j = 0; j < diceArray[i].length; j++) {
+            if (diceArray[i][j] == null) continue;
+            if (rowStr.includes(diceArray[i][j].toString().repeat(3)) && !triple) {
+                rowScore += diceArray[i][j] * 9;
+                break;
+            } else if (rowStr.includes(diceArray[i][j].toString().repeat(2)) && (!triple && !double)) {
+                rowScore += diceArray[i][j] * 4;
+                break;
+            } else {
+                rowScore += diceArray[i][j];
+            }
+        }
+        score += rowScore;
+    }
+    return score;
+}
+
+// BROKEN
+function score2(diceArray) {
     let score = 0;
     diceArray.forEach(column => {
         for(let i = 0; i < column.length; i++) {
